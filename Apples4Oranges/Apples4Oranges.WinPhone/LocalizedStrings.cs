16c1db2aaf489b18e6df7d0dba5667de0a959689
whitewolf33@gmail.com <whitewolf33@gmail.com>
2015-02-16 19:08:30 +1100

    Initial Checkin - Apples4Oranges

---
+++ Apples4Oranges/Apples4Oranges.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Apples4Oranges.WinPhone.Resources;
+
+namespace Apples4Oranges.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ Apples4Oranges/Apples4Oranges/HomePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Apples4Oranges
+{
+    public partial class HomePage : TabbedPage
+    {
+        public HomePage()
+        {
+          InitializeComponent();
+          this.Title = "Apples 4 Oranges";
+
+          this.ItemSource = new string[] { "STUFF", "WHERE", "WANT" };
+          this.ItemTemplate = new DataTemplate(() => { return new ContentPage(); }); 
+        }
+    }
+}
